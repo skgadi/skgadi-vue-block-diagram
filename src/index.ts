b305c63e73af2f6cf2bde19bdf6bbb32ec545602
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2024-09-26 22:54:09 -0600

    Automatic push

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+// Import all the components here, there are more elegant ways to do this but this is the simplest
+import PersonalButton from "./components/PersonalButton.vue";
+export { PersonalButton };
